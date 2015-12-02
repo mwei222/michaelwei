7359402fd70d2371cb33c8197ad4f7edfc4a7a1f
mwei222 <michael.wei@berkeley.edu>
2015-12-02 11:28:14 -0800

    please

---
+++ assets/js/script.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+	$("#cow1").click(function() {
+		$(this).fadeOut(2000,0);
+		}
+	);
+});	
+
+$(document).ready(function(){
+	$("#cow2").click(function() {
+		$(this).fadeOut(2000,0);
+		}
+	);
+});	
+
+$(document).ready(function(){
+	$("#cow3").click(function() {
+		$(this).fadeOut(2000,0);
+		}
+	);
+});	
+
+$(document).ready(function(){
+	$("#cow4").click(function() {
+		$(this).fadeOut(2000,0);
+		}
+	);
+});	
+
+
+$(document).ready(function(){
+	$("#cow5").click(function() {
+		$(this).fadeOut(2000,0);
+		}
+	);
+});	
+
+
+$(document).ready(function(){
+	$("#cow6").click(function() {
+		$(this).fadeOut(2000,0);
+		}
+	);
+});	
+
+$(document).ready(function(){
+	$("#cow7").click(function() {
+		$(this).fadeOut(2000,0);
+		}
+	);
+});	
+
+
+$(document).ready(function(){
+	$("#p1").hover(function(){
+		$("#popup1").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#p2").hover(function(){
+		$("#popup2").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#p3").hover(function(){
+		$("#popup3").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#p4").hover(function(){
+		$("#popup4").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#p5").hover(function(){
+		$("#popup5").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#p6").hover(function(){
+		$("#popup6").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#p7").hover(function(){
+		$("#popup7").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#p8").hover(function(){
+		$("#popup8").removeClass("invis");
+		}
+	);
+});
+
+$(document).ready(function(){
+	$("#cowsell").click(function() {
+		$("#arrows").fadeIn("slow");
+		}
+	);
+});	
+
+$(document).ready(function(){
+	$("#cowsell").click(function() {
+		$("#sold").removeClass("invis");
+		$("#sold").addClass("visible");
+		}
+	);
+});	
+
